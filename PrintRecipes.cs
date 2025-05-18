687c8e14319d430bf9b68387fd929d51e69a01a2
Gil Guereque <gguereque326@gmail.com>
2025-05-17 20:43:55 -0700

    Set up PrintRecipes class

---
+++ PrintRecipes.cs
@@ -0,0 +1,15 @@
+﻿namespace CookieCookbook.PrintRecipes
+{
+    public interface PrintRecipes
+    {
+        public static void PrintRecipe()
+        {
+        }
+
+    }
+
+    public class PrintAvailableRecipes : PrintRecipes
+    {
+        public static PrintAvailableRecipes Instance = new PrintAvailableRecipes();
+    }
+}
