bfe1b90b9e9db756390304442bbf8022769aaa57
Gil Guereque <gguereque326@gmail.com>
2025-05-18 18:12:56 -0700

    Built intitial Json serialization feature for the Recipe object

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using CookieCookbook.IngredientsList;
 using CookieCookbook.PrintIngredients;
+using CookieCookbook.StoreRecipes;
 
 // Main application flow
 namespace CookieCookbook
@@ -12,6 +13,10 @@ namespace CookieCookbook
             
             // Call PrintIngredients method to print list to the console
             PrintAvailableIngredients.PrintIngredients();
+
+            // Testing Serialization of Recipe object to Json file
+            var store = new StoreRecipesInFile();
+            store.SaveAndReadRecipe();
 
             // Program exit
             Console.WriteLine("\nPress any key to exit");
--- StoreRecipesInFile.cs
+++ StoreRecipesInFile.cs
@@ -4,22 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.IO;
 
-namespace CookieCookbook
+namespace CookieCookbook.StoreRecipes
 {
     public class Recipe
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Instructions { get; set; }
-        
     }
-    
+
     public class StoreRecipesInFile
     {
         public StoreRecipesInFile() { }
 
-        const string FileName = "Recipe";
-        const FileFormat FileFormat = FileFormat.Json;
+        private Recipe recipe = new Recipe
+        {
+            Id = 1,
+            Name = "Butter",
+            Instructions = "This is a test recipe. Add to other ingredients"
+        };
+
+        public const string FileName = "Recipe.json";
+
+        public void SaveAndReadRecipe()
+        {
+            // Serialize the recipe object to JSON and save it to a file
+            string jsonString = JsonSerializer.Serialize(recipe);
+            File.WriteAllText(FileName, jsonString);
+
+            // Read the content of the file and print it to the console
+            Console.WriteLine(File.ReadAllText(FileName));
+        }
     }
 }
