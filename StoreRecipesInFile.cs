0a7d7c435a2dc38460a6b048f8278253f2ea4dc3
Gil Guereque <gguereque326@gmail.com>
2025-05-18 17:56:07 -0700

    Creating initial StoreRecipesInFile class

--- StoreRecipesInFile.cs
+++ StoreRecipesInFile.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace CookieCookbook
 {
-    internal class StoreRecipesInFile
+    public class Recipe
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Instructions { get; set; }
+        
+    }
+    
+    public class StoreRecipesInFile
+    {
+        public StoreRecipesInFile() { }
+
+        const string FileName = "Recipe";
+        const FileFormat FileFormat = FileFormat.Json;
     }
 }
