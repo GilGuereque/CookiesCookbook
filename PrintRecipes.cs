1a90ee4bdb9b7163836ca788ebf8673bc1eb82ba
Gil Guereque <gguereque326@gmail.com>
2025-05-18 17:40:58 -0700

    Creating initial version of PrintAvailableRecipes & PrintSingleRecipe classes

--- PrintRecipes.cs
+++ PrintRecipes.cs
@@ -1,15 +1,34 @@
-﻿namespace CookieCookbook.PrintRecipes
+﻿using CookieCookbook.IngredientsList;
+
+namespace CookieCookbook.PrintRecipes
 {
-    public interface PrintRecipes
+    public interface IPrintRecipes
     {
-        public static void PrintRecipe()
+        void PrintRecipe()
         {
         }
 
     }
 
-    public class PrintAvailableRecipes : PrintRecipes
+    public class PrintAvailableRecipes : IPrintRecipes
     {
-        public static PrintAvailableRecipes Instance = new PrintAvailableRecipes();
+        private bool RecipesExist;
+        public void PrintRecipe()
+        {
+            if (RecipesExist)
+                Console.WriteLine($"Existing recipes are: \n" + "*****{N}*****"); //TODO: Recactor method to print out multiple recipes separated by asterisks and number of recipe
+        }
+    }
+
+    public class PrintSingleRecipe : IPrintRecipes
+    {
+        public int NumOfRecipes;
+        public void PrintRecipe()
+        {
+            if (NumOfRecipes == 1)
+            {
+                Console.WriteLine($"RecipeId. RecipeName. RecipeInstructions."); //TODO: Console output the actual Recipe ID. Name. Instructions.
+            }
+        }
     }
 }
