da60d56b9d783a9502969043bee3f20052eccd87
Gil Guereque <gguereque326@gmail.com>
2025-05-30 19:28:56 -0700

    Refactored StringsTextualReposiory to IStringsRepository and moved to DataAccess folder

--- Program.cs
+++ Program.cs
@@ -7,17 +7,56 @@ using CookieCookbook.RecipesUserInteraction;
 using CookieCookbook.StoreRecipes;
 
 
+// Constant to determine file format
+const FileFormat Format = FileFormat.Txt;
+
+IStringsRepository stringsRepository = Format == FileFormat.Json ?
+    new StringsJsonRepository() :
+    new StringsTextualRepository();
+
+// Determine whether the file format will be a json or txt
+const string FileName = "recipe";
+var fileMetadata = new FileMetaData(FileName, Format);
+
+// Instantiate IngredientsRegister object
 var ingredientsRegister = new IngredientsRegister();
 
-// renaming main class 
+// Instantiating cookiesRecipeApp object with all neccessary parameters
 var cookiesRecipesApp = new CookiesRecipesApp(
     new RecipesRepository(
-        new StringsJsonRepository(),
+        stringsRepository,
         ingredientsRegister),
     new RecipesConsoleUserInteraction(
         ingredientsRegister));
 
-cookiesRecipesApp.Run("recipes.json");
+// Run application
+cookiesRecipesApp.Run(fileMetadata.ToPath());
+
+
+public class FileMetaData
+{
+    public string Name { get; }
+    public FileFormat Format { get; }
+    public FileMetaData(string name, FileFormat fileFormat)
+    {
+        Name = name;
+        Format = fileFormat;
+    }
+
+    public string ToPath() => $"{Name}.{Format.AsFileExtension()}";
+}
+
+public static class FileFormatExtensions
+{
+    public static string AsFileExtension(this FileFormat fileFormat) =>
+        fileFormat == FileFormat.Json ? "json" : "txt";
+}
+
+public enum FileFormat
+{
+    Json,
+    Txt 
+}
     
 public class CookiesRecipesApp
 {
@@ -39,21 +78,6 @@ public class CookiesRecipesApp
         _recipesUserInteraction.PromptToCreateRecipe();
 
         var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
-        
-        //Console.WriteLine("Create a new cookie recipe! Available ingredients are:\n");
-            
-        //// Call PrintIngredients method to print list to the console
-        //PrintAvailableIngredients.PrintIngredients();
-
-        //// Testing Serialization of Recipe object to Json file
-        //var store = new StoreRecipesInFile();
-        //store.SaveAndReadRecipe();
-
-        //if (File.Exists(store.FullPath))
-        //{
-        //    Console.WriteLine($"\nExisting recipes are: " + store.FullPath + $"\nFile: \n" + File.ReadAllText(StoreRecipesInFile.FileName));
-        //}
-
 
         // high level design
         if (ingredients.Count() > 0)
@@ -73,11 +97,22 @@ public class CookiesRecipesApp
         }
 
         _recipesUserInteraction.Exit();
-
-        // Program exit
-
-
     }
+
+    // FIRST SOLUTION ITERATION
+    //Console.WriteLine("Create a new cookie recipe! Available ingredients are:\n");
+
+    //// Call PrintIngredients method to print list to the console
+    //PrintAvailableIngredients.PrintIngredients();
+
+    //// Testing Serialization of Recipe object to Json file
+    //var store = new StoreRecipesInFile();
+    //store.SaveAndReadRecipe();
+
+    //if (File.Exists(store.FullPath))
+    //{
+    //    Console.WriteLine($"\nExisting recipes are: " + store.FullPath + $"\nFile: \n" + File.ReadAllText(StoreRecipesInFile.FileName));
+    //}
 }
 
 
